cbc17c0049ec31d13ae9308e39e60cc1cfd8aa59
Melania Morales <melaniacrc@gmail.com>
2021-09-01 20:04:12 -0600

    U: Only reads XML files

--- Indexing.py
+++ Indexing.py
@@ -1,16 +1,20 @@
 import re
+import os
 
 #Funcion que abre y lee y retorna el texto de un archivo
 def readFile(path):
-    try:
-        with open(path, encoding='utf-8') as file:
-            content=file.read()
-            file.close()
-    except:
-        with open(path,"r") as file:
-            content=file.read()
-            file.close()
-    return content
+    route, extension = os.path.splitext(path)
+    if(extension==".xml" or extension==".XML"):    
+        try:
+            with open(path, encoding='utf-8') as file:
+                content=file.read()
+                file.close()
+        except:
+            with open(path,"r") as file:
+                content=file.read()
+                file.close()
+        return content
+    return ""
 
 #Funcion que elimina los signos de puntuacion del texto y los cambia por un espacio en blanco
 def deletePunctuation(text):
