17ecccf8e7bd519f35e3543fd68c577e179ac350
Melania Morales <melaniacrc@gmail.com>
2021-09-01 22:15:12 -0600

    C: Stopwords

--- Indexing.py
+++ Indexing.py
@@ -31,13 +31,26 @@ def deleteSpecialCharacters(text):
     text = text.translate(mapTable)
     return str(text)
 
+#Funcion que toma un texto y lo separa en palabras
+def splitText(text):
+    return text.split()
+
 #Eliminar stopwords
-def stopwords(list, text):
-    return ''
+def stopwords(stopwordsList, wordList):
+    for stopWord in stopwordsList:
+        while stopWord in wordList: wordList.remove(stopWord)
+    return wordList   
+
 
 text = readFile("C:/Users/melan/OneDrive/6. TEC-SEXTO SEMESTRE/RECUPERACION DE INFORMACION TEXTUAL/PROYECTO 1/xml-es/apx-authors.xml")
-text=deleteTags(text)
-text=deletePunctuation(text)
-text=deleteSpecialCharacters(text)
-print(deleteSpecialCharacters(text))
-#stopwords(["a","ante","bajo","cabe","con","contra","de","desde"],text)
+text = deleteTags(text)
+text = deletePunctuation(text)
+text = deleteSpecialCharacters(text)
+text = deleteSpecialCharacters(text)
+wordList = splitText(text)
+print(wordList)
+
+stopwordsList = ["a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "e", "el", "en", "entre", "hacia", "hasta", "ni", "la", "le", "lo", "los", "las", "o", "para", "pero", "por", "que", "se", "segun", "sin", "so", "sobre", "tras", "u", "un", "una", "unas", "uno", "unos", "y"]
+
+wordList = stopwords(stopwordsList,wordList)
+print(wordList)
