782ea94e591994a73530f01079e2e8955c762239
Melania Morales <melaniacrc@gmail.com>
2021-09-06 19:54:52 -0600

    U: Index class

--- index.py
+++ index.py
@@ -7,211 +7,215 @@ import re
 import os
 
 
-def takePath():
-    path = input("Indique el path donde se encuentra su archivo y el nombre del mismo: ")
-    return path
-
-#PROCESAMIENTO DE LA COLECCION
-collection = {
-    'N':0,
-    'avgLen':0,
-    'ruta':""
-}
-
-documents={
-}
-
-collectionDictionary={
-}
-
-#Toma todos los paths de los documentos en una coleccion
-def documentsInDirectory(ruta):
-    return [f for f in os.listdir(ruta) if os.path.isfile(os.path.join(ruta, f))]
-
-
-#Procesa archivo por archivo para crear el archivo invertido y el track de documentos
-def processCollection(path):
-    #path="C:/Users/Laptop/OneDrive/Documentos/Sexto Semestre/RECUPERACION DE INFORMACION TEXTUAL/Pruebas"
-    path="C:/Users/melan/OneDrive/6. TEC-SEXTO SEMESTRE/RECUPERACION DE INFORMACION TEXTUAL/PROYECTO 1/pruebas"
-    paths = documentsInDirectory(path)
+class Index:
     
-    for p in paths:
-        dictionary = dictionaryOfDocument(path+"/"+p)
-        keysOfDictionary = sorted(dictionary.keys())
-        frequencies = sumFrequencies(dictionary.values())
+    def __init__(self):
+        self.documents={
+        }
         
+        self.collection = {
+            'N':0,
+            'avgLen':0,
+            'ruta':""
+        }
 
-        #Update collection
+        self.collectionDictionary={
+        }
+
+        self.stopwordsList=[]
+
+    def takePath(self):
+        path = input("Indique el path donde se encuentra su archivo y el nombre del mismo: ")
+        return path
+
+    #PROCESAMIENTO DE LA COLECCION
+    
+
+    #Toma todos los paths de los documentos en una coleccion
+    def documentsInDirectory(self, ruta):
+        return [f for f in os.listdir(ruta) if os.path.isfile(os.path.join(ruta, f))]
+
+
+    #Procesa archivo por archivo para crear el archivo invertido y el track de documentos
+    def processCollection(self, path):
+        #path="C:/Users/Laptop/OneDrive/Documentos/Sexto Semestre/RECUPERACION DE INFORMACION TEXTUAL/Pruebas"
+        path="C:/Users/melan/OneDrive/6. TEC-SEXTO SEMESTRE/RECUPERACION DE INFORMACION TEXTUAL/PROYECTO 1/pruebas"
+        paths = self.documentsInDirectory(path)
+        
+        for p in paths:
+            dictionary = self.dictionaryOfDocument(path+"/"+p)
+            keysOfDictionary = sorted(dictionary.keys())
+            frequencies = self.sumFrequencies(dictionary.values())
+            
+
+            #Update collection
+            collectionAux = {
+                'N':self.collection.get('N')+1,
+                'avgLen':self.collection.get('avgLen')+frequencies,
+                'ruta':path
+            }
+            self.collection.update(collectionAux)
+
+            #Update documents
+            documentData={
+                'ruta':p,
+                'longuitud':frequencies,
+                'norma':"",
+            }
+            self.documents[self.collection.get('N')] = documentData
+
+            #Update collection dictionary
+            self.updateCollectionDictionary(dictionary, keysOfDictionary)
+        
         collectionAux = {
-            'N':collection.get('N')+1,
-            'avgLen':collection.get('avgLen')+frequencies,
+            'N':self.collection.get('N'),
+            'avgLen':self.collection.get('avgLen')/self.collection.get('N'),
             'ruta':path
         }
-        collection.update(collectionAux)
 
-        #Update documents
-        documentData={
-            'ruta':p,
-            'longuitud':frequencies,
-            'norma':"",
+        self.collection.update(collectionAux)
+        
+        self.calculateWeight()
+        
+        #IDF    
+        self.getIdf()
+
+        #Norma
+        self.updateDocuments()
+
+        print (self.collection)
+        print("///////////////////////////////////////////////////////////")
+        print (self.documents)
+        print("///////////////////////////////////////////////////////////")
+        print(self.collectionDictionary)
+
+    def updateCollectionDictionary(self, dictionary, keys):
+        for term in keys:
+            if term in self.collectionDictionary:
+                self.collectionDictionary[term]['ni'] += 1
+                posting = self.createPosting(dictionary[term])
+                self.collectionDictionary[term]['postings'].update({self.collection['N']:posting})
+            else:
+                termInCollectionDictionary={
+                    'ni':1,
+                    'idfs':0,
+                    'postings':{},
+                }
+                posting = self.createPosting(dictionary[term])
+                termInCollectionDictionary['postings'][self.collection['N']] =  posting
+                self.collectionDictionary[term]=termInCollectionDictionary
+        
+
+    def createPosting(self, term):
+        postingAux={
+            'd':self.collection['N'],
+            'freq':term,
+            'peso':0
         }
-        documents[collection.get('N')] = documentData
+        return postingAux
 
-        #Update collection dictionary
-        updateCollectionDictionary(dictionary, keysOfDictionary)
-    
-    collectionAux = {
-        'N':collection.get('N'),
-        'avgLen':collection.get('avgLen')/collection.get('N'),
-        'ruta':path
-    }
+    def getIdf(self):
+        N = self.collection['N']
+        for term in self.collectionDictionary:
+            ni = self.collectionDictionary[term]['ni']
+            tempIdf = log((N-ni+0.5)/(ni+0.5),10)
+            if tempIdf < 0:
+                self.collectionDictionary[term]['idfs'] = 0
+            else:
+                self.collectionDictionary[term]['idfs'] = tempIdf
 
-    collection.update(collectionAux)
-    
-    calculateWeight()
-    
-    #IDF    
-    getIdf()
+    def updateDocuments(self):
+        for i in range (1,self.collection.get('N')+1):
+            pesosTemp = []
+            for term in self.collectionDictionary.copy():
+                if i in self.collectionDictionary[term]['postings']: 
+                    pesosTemp += [self.collectionDictionary[term]['postings'][i]['peso']]
+            self.documents[i]['norma'] = self.calculateNorma(pesosTemp)
+        
+        
+    def calculateNorma(self,pesosTemp):
+        sum = 0
+        for peso in pesosTemp:
+            sum += pow(peso,2)
+        return sqrt(sum)
 
-    #Norma
-    updateDocuments()
-
-    print (collection)
-    print("///////////////////////////////////////////////////////////")
-    print (documents)
-    print("///////////////////////////////////////////////////////////")
-    print(collectionDictionary)
-
-def updateCollectionDictionary(dictionary, keys):
-    for term in keys:
-        if term in collectionDictionary:
-            collectionDictionary[term]['ni'] += 1
-            posting = createPosting(dictionary[term])
-            collectionDictionary[term]['postings'].update({collection['N']:posting})
-        else:
-            termInCollectionDictionary={
-                'ni':1,
-                'idfs':0,
-                'postings':{},
-            }
-            posting = createPosting(dictionary[term])
-            termInCollectionDictionary['postings'][collection['N']] =  posting
-            collectionDictionary[term]=termInCollectionDictionary
-    
-
-def createPosting(term):
-    postingAux={
-        'd':collection['N'],
-        'freq':term,
-        'peso':0
-    }
-    return postingAux
-
-def getIdf():
-    N = collection['N']
-    for term in collectionDictionary:
-        ni = collectionDictionary[term]['ni']
-        tempIdf = log((N-ni+0.5)/(ni+0.5),10)
-        if tempIdf < 0:
-          collectionDictionary[term]['idfs'] = 0
-        else:
-           collectionDictionary[term]['idfs'] = tempIdf
-
-def updateDocuments():
-    for i in range (1,collection.get('N')+1):
-        pesosTemp = []
-        for term in collectionDictionary.copy():
-            if i in collectionDictionary[term]['postings']: 
-                pesosTemp += [collectionDictionary[term]['postings'][i]['peso']]
-        documents[i]['norma'] = calculateNorma(pesosTemp)
-    
-    
-def calculateNorma(pesosTemp):
-    sum = 0
-    for peso in pesosTemp:
-        sum += pow(peso,2)
-    return sqrt(sum)
-
-def calculateWeight():
-    for term in collectionDictionary:
-        ni = collectionDictionary[term]['ni']
-        for post in collectionDictionary[term]['postings']:
-            frequency = collectionDictionary[term]['postings'][post]['freq']
-            collectionDictionary[term]['postings'][post]['peso'] = log((1+frequency),2)* log((collection['N']/ni),2)
+    def calculateWeight(self):
+        for term in self.collectionDictionary:
+            ni = self.collectionDictionary[term]['ni']
+            for post in self.collectionDictionary[term]['postings']:
+                frequency = self.collectionDictionary[term]['postings'][post]['freq']
+                self.collectionDictionary[term]['postings'][post]['peso'] = log((1+frequency),2)* log((self.collection['N']/ni),2)
 
 
-#PROCESAMIENTO DEL DOCUMENTO
+    #PROCESAMIENTO DEL DOCUMENTO
 
-#Funcion que elimina los tags XML del texto y los cambia por un espacio en blanco
-def deleteTags(text):
-    return re.sub('\<(.*?)\>',' ',text)
+    #Funcion que elimina los tags XML del texto y los cambia por un espacio en blanco
+    def deleteTags(self, text):
+        return re.sub('\<(.*?)\>',' ',text)
 
-#Funcion que elimina los signos de puntuacion del texto y los cambia por un espacio en blanco
-def deletePunctuation(text):
-    return re.sub('[\W]+',' ',text)
+    #Funcion que elimina los signos de puntuacion del texto y los cambia por un espacio en blanco
+    def deletePunctuation(self, text):
+        return re.sub('[\W]+',' ',text)
 
-#Funcion que elimina los caracteres especiales, por ejemplo, las tildes
-def deleteAccent(text):
-    text=text.lower()
-    mapTable = text.maketrans("áéíóúü", "aeiouu")
-    text = text.translate(mapTable)
-    return str(text)
+    #Funcion que elimina los caracteres especiales, por ejemplo, las tildes
+    def deleteAccent(self, text):
+        text=text.lower()
+        mapTable = text.maketrans("áéíóúü", "aeiouu")
+        text = text.translate(mapTable)
+        return str(text)
 
-#Funcion que toma un texto y lo separa en palabras
-def splitText(text):
-    return text.split()
+    #Funcion que toma un texto y lo separa en palabras
+    def splitText(self, text):
+        return text.split()
 
-#Eliminar stopwords
-def stopwords(stopwordsList, wordList):
-    for stopWord in stopwordsList:
-        while stopWord in wordList: wordList.remove(stopWord)
-    return wordList   
+    #Eliminar stopwords
+    def stopwords(self, stopwordsList, wordList):
+        for stopWord in stopwordsList:
+            while stopWord in wordList: wordList.remove(stopWord)
+        return wordList   
 
-#Funcion la lista de palabras y quita duplicados
-def wordsInTextList(wordList):
-	return list(set(wordList))
+    #Funcion la lista de palabras y quita duplicados
+    def wordsInTextList(self, wordList):
+        return list(set(wordList))
 
-#Funcion que toma una lista de palabras y nos devuelve un diccionario que contiene cada palabra y la cantidad de veces que aparece
-def wordAppearances(list):
-    return Counter(list)
+    #Funcion que toma una lista de palabras y nos devuelve un diccionario que contiene cada palabra y la cantidad de veces que aparece
+    def wordAppearances(self, list):
+        return Counter(list)
 
-#Funcion que toma todas las frecuencias y las suma
-def sumFrequencies(list):
-    result = 0
-    for i in list: 
-        result+=i
-    return result
+    #Funcion que toma todas las frecuencias y las suma
+    def sumFrequencies(self, list):
+        result = 0
+        for i in list: 
+            result+=i
+        return result
 
-#Funcion que toma los stopwords de un archivo y los procesaa en una lista
-def getStopwords(path):
-    text = FileManager.readFile(path)
-    text = re.sub('\n+|\s+',',',text)
-    text = text.split(",")
-    return text
+    #Funcion que toma los stopwords de un archivo y los procesaa en una lista
+    def getStopwords(self, path):
+        text = FileManager.readFile(path)
+        text = re.sub('\n+|\s+',',',text)
+        text = text.split(",")
+        return text
 
-#Funcion principal
-def dictionaryOfDocument(path):
-    #Text Input
-    text = FileManager.readFile(path)
+    #Funcion principal
+    def dictionaryOfDocument(self, path):
+        #Text Input
+        text = FileManager.readFile(path)
 
-    #Text format
-    text = deleteTags(text)
-    text = deletePunctuation(text)
-    text = deleteAccent(text)
+        #Text format
+        text = self.deleteTags(text)
+        text = self.deletePunctuation(text)
+        text = self.deleteAccent(text)
 
-    #Final format list
-    wordList = splitText(text)
+        #Final format list
+        wordList = self.splitText(text)
 
-    stopwordsList = getStopwords("C:/Users/melan/OneDrive/6. TEC-SEXTO SEMESTRE/RECUPERACION DE INFORMACION TEXTUAL/PROYECTO 1/pruebas/stopwords.txt")
+        stopwordsList = self.getStopwords("C:/Users/melan/OneDrive/6. TEC-SEXTO SEMESTRE/RECUPERACION DE INFORMACION TEXTUAL/PROYECTO 1/pruebas/stopwords.txt")
 
-    wordList = stopwords(stopwordsList,wordList)
+        wordList = self.stopwords(stopwordsList,wordList)
 
-    wordList = wordsInTextList(wordList)
-    wordList.sort()
+        wordList = self.wordsInTextList(wordList)
+        wordList.sort()
 
-    wordIndex = wordAppearances(wordList)
-    
-    return wordIndex
-
-path = takePath()
-processCollection(path)
+        wordIndex = self.wordAppearances(wordList)
+        
+        return wordIndex
